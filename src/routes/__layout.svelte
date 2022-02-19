6b5be1b25415e3b9c738e4ae357a372999a33a57
OllieFord <ollie.ford343@gmail.com>
2022-02-19 21:03:01 +0100

    fade page transition

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,33 @@
+<script context="module">
+	export const load = ({ url }) => {
+		const currentRoute = url.pathname;
+
+		return {
+			props: {
+				currentRoute
+			}
+		};
+	};
+</script>
+
 <script>
 	import '../app.css';
 	import Header from '$lib/components/layout/header.svelte';
 	import Footer from '$lib/components/layout/footer.svelte';
+
+	import { fade } from 'svelte/transition';
+
+	export let currentRoute;
 </script>
 
 <Header />
-<main class="container max-w-3xl mx-auto px-4 my-4 ">
-	<slot />
-</main>
-
-<Footer />
+{#key currentRoute}
+	<main
+		in:fade={{ duration: 150, delay: 150 }}
+		out:fade={{ duration: 150 }}
+		class="container max-w-3xl mx-auto px-4 my-4 "
+	>
+		<slot />
+	</main>
+	<Footer />
+{/key}
