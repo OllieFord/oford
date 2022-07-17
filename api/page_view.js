5701af8bd392758df4c59032f6c07a7092ff70bf
OllieFord <ollie.ford343@gmail.com>
2022-07-17 15:20:55 +0200

    new kesys

--- api/page_view.js
+++ api/page_view.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.supaUrl, process.env.supaKey)
+const supabase = createClient(process.env.VITE_SVELTE_APP_SUPABASE_URL, process.env.supaKey)
 
 supabase.rpc('increment_stats', { row_id: 1 }).then((res) => {
     console.log(res);
