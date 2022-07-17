c94502ab0993057f03e7ceb87ae6f780693f6485
OllieFord <ollie.ford343@gmail.com>
2022-07-17 15:25:45 +0200

    serverless tweaks

--- api/page_view.js
+++ api/page_view.js
@@ -1,7 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.VITE_SVELTE_APP_SUPABASE_URL, process.env.supaKey)
-
+let resultApi;
 supabase.rpc('increment_stats', { row_id: 1 }).then((res) => {
-    console.log(res);
+    result = res;
 });
+
+
+module.exports = (req, res) => {
+    res.json({
+        result: resultApis
+    })
+
+}
