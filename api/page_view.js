3876ca58bef7069856c6968cfb7b4b683bf08a26
OllieFord <ollie.ford343@gmail.com>
2022-07-17 16:19:18 +0200

    export function

--- api/page_view.js
+++ api/page_view.js
@@ -1,15 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.VITE_SVELTE_APP_SUPABASE_URL, process.env.supaKey)
-let resultApi;
-supabase.rpc('increment_stats', { row_id: 1 }).then((res) => {
-    result = res;
-});
 
-
-module.exports = (req, res) => {
-    res.json({
-        result: resultApis
-    })
-
+export default function increment_page_view(request, response) {
+    const supabase = createClient(process.env.VITE_SVELTE_APP_SUPABASE_URL, process.env.supaKey)
+    let resultApi;
+    supabase.rpc('increment_stats', { row_id: 1 }).then((res) => {
+        resultApi = res;
+        return response.status(200).json({ resultApi });
+    });
 }
